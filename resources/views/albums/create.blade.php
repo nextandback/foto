ac3e9243585afa39d1866472d11165d25c539bf6
Mehmet Dogukan Sevinc <nextandbackacademy@gmail.com>
2020-07-18 01:27:57 +0300

    LAST

---
+++ resources/views/albums/create.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+    <h2>Create New Album</h2>
+
+    <form method="post" action="{{ route('album-store') }}" enctype="multipart/form-data">
+        @csrf
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" id="name" name="name" placeholder="Enter Name">
+        </div>
+        <div class="form-group">
+            <label for="description">Description</label>
+            <input type="text" class="form-control" id="description" name="description" placeholder="Enter description">
+        </div>
+        <div class="form-group">
+            <label for="cover-image">Cover Image</label>
+            <input type="file" class="form-control" id="cover-image" name="cover-image" >
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="/css/app.css">
+
+    <title>Photoshow</title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
